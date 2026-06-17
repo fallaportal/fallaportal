95c15b81f79a09883494d38968fd05d25470a08a
fallaportal <it.fallaportal@gmail.com>
2026-06-17 09:39:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Falla Portal — Service Worker v4.0.1
-const BUILD_VERSION = '4019';
+const BUILD_VERSION = '4020';
 const CACHE_NAME = 'falla-portal-v' + BUILD_VERSION;
 const BASE = '/fallaportal';
 
